eb5d35505dabe566479a4a5b107be2e358f31eb2
Bianca Aspin <bianca@mj94.net>
2022-12-15 13:35:57 -0800

    commit tree f235251a0a417a3b57a8d5aceb27e0f00f361c12

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// Test 1: Removing the <main>
+const main = document.querySelector("main#main");
+main.remove();
+
+// Test 2: Create a new header
+const newHeader = document.createElement("h1");
+
+// Test 3: Assign id to the new <h1>
+newHeader.id = "victory";
+
+// Test 4: Assign text to new <h1>
+newHeader.textContent = "Bianca is the champion";
