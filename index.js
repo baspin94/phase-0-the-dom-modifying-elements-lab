c3ed240d2968adc40c02c26fda1763a62bd6e603
Bianca Aspin <bianca@mj94.net>
2023-01-10 16:50:27 -0800

    Recompleted JS for review in Phase 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+// Test #1
+const removeThis = document.querySelector("#main");
+removeThis.remove();
+
+// Test #2
+const newHeader = document.createElement("h1");
+
+// Test #3
+newHeader.id = 'victory';
+
+// Test #4
+newHeader.textContent = "Bianca is the champion"
+document.body.append(newHeader);
